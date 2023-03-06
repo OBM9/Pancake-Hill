f5ea4ff3fe3227a92578252d5b20af1acdf909f1
OBM9 <TheLeviathan12@hotmail.com>
2023-03-06 02:06:32 -0700

    commit

--- main.js
+++ main.js
@@ -11,8 +11,6 @@ console.log(b);
 var someNumber = 23;
 console.log(someNumber);
 
-document.getElementById('someText').innerHTML = age;
-
 var age = prompt('What is your age?');
-
+document.getElementById('someText').innerHTML = age;
 
