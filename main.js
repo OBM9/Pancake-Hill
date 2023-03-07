7213a609a5fca9a9883da0230e5fe878f0ba04a9
OBM9 <TheLeviathan12@hotmail.com>
2023-03-07 01:16:40 -0700

    commit

--- main.js
+++ main.js
@@ -4,23 +4,8 @@ console.log('hello');
 //how to write a comment inline
 
 //variables
-
-var b = 'smoothie';
-console.log(b);
-
-var someNumber = 23;
-//console.log(someNumber);
-
-//var age = prompt('What is your age?');
-//document.getElementById('someText').innerHTML = age;
-
-//Numbers in javascript
-//Functions
-function fun() {
-  alert('This is a function');
-}
-fun();
-
+function fun() 
+ 
 
 
 
