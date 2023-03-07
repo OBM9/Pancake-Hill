9aff79499826efe9c0847e9d091131965a1f87b0
OBM9 <TheLeviathan12@hotmail.com>
2023-03-07 01:41:19 -0700

    commit

--- main.js
+++ main.js
@@ -4,14 +4,17 @@ console.log('hello');
 //how to write a comment inline
 
 //variables
-function greeting() {
-  var name =prompt('What is your name?');
-  var result = 'Hello' + name;
+//function greeting() {
+ // var name =prompt('What is your name?');
+ // var result = 'Hello' + name;
   console.log(result);
 } 
   greeting();
-  function someNumbers(num1, num2);
-  
+  function someNumbers(num1, num2) {
+    var result = num1 + num2;
+    console.log(result);
+  }
+
 
 
 
